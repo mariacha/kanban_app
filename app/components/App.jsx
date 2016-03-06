2e9b19ddbd6a31579fcd46bde669af4779d28686
Maria Fisher <mariacha@gmail.com>
2016-03-06 12:20:38 -0800

    Refactoring and adding ability to edit notes.

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -1,6 +1,8 @@
 import uuid from 'node-uuid';
 import React from 'react';
 
+import Notes from './Notes.jsx';
+
 export default class App extends React.Component {
   constructor(props) {
     super(props);
@@ -28,9 +30,7 @@ export default class App extends React.Component {
     return (
       <div>
         <button onClick={this.addNote}>+</button>
-        <ul>{notes.map(note =>
-            <li key={note.id}>{note.task}</li>
-        )}</ul>
+        <Notes notes={notes} onEdit={this.editNote} />
       </div>
     );
   }
@@ -58,5 +58,22 @@ export default class App extends React.Component {
         task: 'New task'
       }])
     });
+  };
+
+  editNote = (id, task) => {
+    // Don't modify if trying set an empty value
+    if(!task.trim()) {
+      return;
+    }
+
+    const notes = this.state.notes.map(note => {
+      if(note.id === id && task) {
+        note.task = task;
+      }
+
+      return note;
+    });
+
+    this.setState({notes});
   };
 }
