e458e4778fa7273df3b5e488bc1401aecb8f0f50
Maria Fisher <mariacha@gmail.com>
2016-03-06 10:44:17 -0800

    Adding auto-detect of changes to the webpack.config.js file that require new packages.

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,10 @@ const path = require('path');
 const merge = require('webpack-merge');
 const webpack = require('webpack');
 
+// Auto-senses if I've made a change to the requires above and adds them to
+// the package.json file, I think.
+const NpmInstallPlugin = require('npm-install-webpack-plugin');
+
 const TARGET = process.env.npm_lifecycle_event;
 
 const PATHS = {
@@ -62,7 +66,10 @@ if(TARGET === 'start' || !TARGET) {
       port: process.env.PORT
     },
     plugins: [
-      new webpack.HotModuleReplacementPlugin()
+      new webpack.HotModuleReplacementPlugin(),
+      new NpmInstallPlugin({
+        save: true // --save
+      })
     ]
   });
 }
