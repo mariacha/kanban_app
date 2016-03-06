7d0cbebca4cb3a0e070b3decc35155868d3cd7b3
Maria Fisher <mariacha@gmail.com>
2016-03-06 09:52:18 -0800

    Creating the first build.js file.

--- app/component.js
+++ app/component.js
@@ -1,5 +1,7 @@
 /**
  * Created by mfisher on 3/6/16.
+ *
+ * component.js. File for adding an h1 to the app div.
  */
 module.exports = function () {
   var element = document.createElement('h1');
--- app/index.js
+++ app/index.js
@@ -1,5 +1,7 @@
 /**
  * Created by mfisher on 3/6/16.
+ *
+ * index.js file for creating the app div.
  */
 var component = require('./component');
 var app = document.createElement('div');
