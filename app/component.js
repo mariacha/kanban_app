bb039fa35e0b0d46b457243def242e6ca09a7bfa
Maria Fisher <mariacha@gmail.com>
2016-03-06 09:25:14 -0800

    Creating a component to write hello world and pointing index.js at it.

---
+++ app/component.js
@@ -0,0 +1,10 @@
+/**
+ * Created by mfisher on 3/6/16.
+ */
+module.exports = function () {
+  var element = document.createElement('h1');
+
+  element.innerHTML = 'Hello world';
+
+  return element;
+};
---
+++ app/index.js
@@ -0,0 +1,9 @@
+/**
+ * Created by mfisher on 3/6/16.
+ */
+var component = require('./component');
+var app = document.createElement('div');
+
+document.body.appendChild(app);
+
+app.appendChild(component());
