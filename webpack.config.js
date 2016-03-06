ba946ccd1cb8b776c85b527a9ab253224c9f7944
Maria Fisher <mariacha@gmail.com>
2016-03-06 11:00:27 -0800

    Getting ready to use babel.

--- webpack.config.js
+++ webpack.config.js
@@ -23,6 +23,12 @@ const common = {
     path: PATHS.build,
     filename: 'bundle.js'
   },
+  // Add resolve.extensions for babel.
+  // '' is needed to allow imports without an extension.
+  // Note the .'s before extensions as it will fail to match without!!!
+  resolve: {
+    extensions: ['', '.js', '.jsx']
+  },
   module: {
     loaders: [
       {
@@ -31,6 +37,17 @@ const common = {
         // in css through @imports (css) first, then requires (js).
         test: /\.css$/,
         loaders: ['style', 'css'],
+        include: PATHS.app
+      },
+      // Set up jsx. This accepts js too thanks to RegExp
+      {
+        test: /\.jsx?$/,
+        // Enable caching for improved performance during development
+        // It uses default OS directory by default. If you need something
+        // more custom, pass a path to it. I.e., babel?cacheDirectory=<path>
+        loaders: ['babel?cacheDirectory'],
+        // Parse only app files! Without this it will go through entire project.
+        // In addition to being slow, that will most likely result in an error.
         include: PATHS.app
       }
     ]
