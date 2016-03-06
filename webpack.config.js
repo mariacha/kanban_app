100362994b28cc08cff9f551e35548fc327a5a04
Maria Fisher <mariacha@gmail.com>
2016-03-06 10:17:56 -0800

    Allowing different scripts to load different files.

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,13 @@
-const path = require('path');
+const merge = require('webpack-merge');
+
+const TARGET = process.env.npm_lifecycle_event;
 
 const PATHS = {
   app: path.join(__dirname, 'app'),
   build: path.join(__dirname, 'build')
 };
 
-module.exports = {
+const common = {
   // Entry accepts a path or an object of entries. We'll be using the
   // latter form given it's convenient with more complex configurations.
   entry: {
@@ -16,3 +18,13 @@ module.exports = {
     filename: 'bundle.js'
   }
 };
+
+// Default configuration (for dev).
+if(TARGET === 'start' || !TARGET) {
+  module.exports = merge(common, {});
+}
+
+// Configuration for the finished product.
+if(TARGET === 'build') {
+  module.exports = merge(common, {});
+}
