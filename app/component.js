c58fba64bbd7d76fa3b8cfbf8308f2fa69becee2
Maria Fisher <mariacha@gmail.com>
2016-03-06 10:26:47 -0800

    Adding hot reloading.

--- app/component.js
+++ app/component.js
@@ -6,7 +6,7 @@
 module.exports = function () {
   var element = document.createElement('h1');
 
-  element.innerHTML = 'Hello world';
+  element.innerHTML = 'Hello world!!';
 
   return element;
 };
