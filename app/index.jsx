4f581378d3c03104038c9fcdc6cb056ff7dc5f6d
Maria Fisher <mariacha@gmail.com>
2016-03-06 11:16:13 -0800

    Moving the app to being based on React.

---
+++ app/index.jsx
@@ -0,0 +1,12 @@
+/**
+ * Created by mfisher on 3/6/16.
+ *
+ * index.jsx file for creating the app div.
+ */
+import './main.css';
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/App.jsx';
+
+ReactDOM.render(<App />, document.getElementById('app'));
