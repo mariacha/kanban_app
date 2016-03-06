a888d25450cbcd26e940f6f4f786fe25ab48f27f
Maria Fisher <mariacha@gmail.com>
2016-03-06 13:58:35 -0800

    Beginning to refactor for alt implementation.

---
+++ app/actions/NoteActions.js
@@ -0,0 +1,8 @@
+/**
+ * Created by mfisher on 3/6/16.
+ *
+ * Sets up a CRUD api for Notes.
+ */
+import alt from '../libs/alt';
+
+export default alt.generateActions('create', 'update', 'delete');
---
+++ app/libs/alt.js
@@ -0,0 +1,12 @@
+/**
+ * Created by mfisher on 3/6/16.
+ *
+ * Sets up an alt instance.
+ */
+import Alt from 'alt';
+//import chromeDebug from 'alt-utils/lib/chromeDebug';
+
+const alt = new Alt();
+//chromeDebug(alt);
+
+export default alt;
