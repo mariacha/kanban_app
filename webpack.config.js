ca1926e69ceb1c8eebcccda6f8ea3ac00380235e
Maria Fisher <mariacha@gmail.com>
2016-03-06 13:39:55 -0800

    A few small changes to match the D3 graphing app I'm working on.

--- webpack.config.js
+++ webpack.config.js
@@ -22,15 +22,15 @@ const common = {
   entry: {
     app: PATHS.app
   },
-  output: {
-    path: PATHS.build,
-    filename: 'bundle.js'
-  },
   // Add resolve.extensions for babel.
   // '' is needed to allow imports without an extension.
   // Note the .'s before extensions as it will fail to match without!!!
   resolve: {
     extensions: ['', '.js', '.jsx']
+  },
+  output: {
+    path: PATHS.build,
+    filename: 'bundle.js'
   },
   module: {
     loaders: [
